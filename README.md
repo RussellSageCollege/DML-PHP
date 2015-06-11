6003d8f46683fdd369939d52ed5f0a015692082b
Alex Markessinis <markea@almyz125.com>
2015-06-11 10:15:39 -0400

    initalized

---
+++ README.md
@@ -0,0 +1,3 @@
+# WinDSX wrapper class.
+
+This class wraps a few functions around the commands in WinDSX's markup language.
---
+++ WinDSX.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 6/11/15
+ * Time: 9:57 AM
+ *
+ * This is a wrapper around the WinDSX Markup Language, http://www.dsxinc.com/Docs/databasemgmtapi.pdf
+ */
+class WinDSX
+{
+
+    /**
+     * @param string $identifier
+     * @return string
+     */
+    public function setID($identifier = '')
+    {
+        return 'I L1 U1 ^' . $identifier . '^^^';
+    }
+
+    /**
+     * @param string $table_name
+     * @return string
+     */
+    public function setTable($table_name = '')
+    {
+        return 'T ' . $table_name;
+    }
+
+    /**
+     * @param string $field_name
+     * @param string $field_value
+     * @return string
+     */
+    public function setField($field_name = '', $field_value = '')
+    {
+        return 'F ' . $field_name . ' ^' . $field_value . '^^^';
+    }
+
+    /**
+     * @return string
+     */
+    public function cmd_write()
+    {
+        return 'W';
+    }
+
+    /**
+     * @return string
+     */
+    public function cmd_delete()
+    {
+        return 'D';
+    }
+
+    /**
+     * @return string
+     */
+    public function cmd_update()
+    {
+        return 'U';
+    }
+
+    /**
+     * @return string
+     */
+    public function cmd_print()
+    {
+        return 'P';
+    }
+
+}
